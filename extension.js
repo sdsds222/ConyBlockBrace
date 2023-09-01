f9849126497661764564bb258267971eb698bc87
sdsds222 <15674581745@163.com>
2023-09-01 13:54:21 +0800

    更新

--- extension.js
+++ extension.js
@@ -5,7 +5,13 @@ const vscode = require('vscode');
  */
 
 var updateDecorationsFunction;
-let highlightingEnabled = false; // 默认关闭高亮
+
+
+var highlightingEnabled = false; // 默认关闭高亮
+
+var theNum = 3;
+
+var colorMode = false;
 
 function toggleHighlighting() {
   console.log("ConyBlockBrace启用状态:" + highlightingEnabled);
@@ -19,12 +25,114 @@ function toggleHighlighting() {
 }
 
 
+
 function activate(context) {
   console.log('ConyBlockBrace正在运行');
   // 注册 ToggleHighlighting 命令
   context.subscriptions.push(
     vscode.commands.registerCommand('ConyBlockBrace.ConyToggleHighlighting', toggleHighlighting)
   );
+  // 获取用户配置项
+  const config = vscode.workspace.getConfiguration('ConyBlockBrace');
+
+  //获取用户配置的自启动选项
+  highlightingEnabled = config.get('enabled', false); // 默认为 false
+
+  //获取用户配置的自启动选项
+  colorMode = config.get('ColorModeEnabled', false); // 默认为 false
+
+  // 获取用户配置的颜色值和是否开启功能的选项
+  const blockColors = config.get('blockColors', []);
+  const blockDecorators = blockColors.map(color => vscode.window.createTextEditorDecorationType({
+    backgroundColor: color
+  }));
+  // 注册增加数字的命令
+  const increaseNumCommand = vscode.commands.registerCommand('ConyBlockBrace.increaseNum', () => {
+    theNum++;
+    if (theNum < 3) {
+      theNum = 3;
+    }
+
+    updateDecorationsFunction();
+    vscode.window.showInformationMessage(`theNum increased to ${theNum}`);
+  });
+
+  // 注册减少数字的命令
+  const decreaseNumCommand = vscode.commands.registerCommand('ConyBlockBrace.decreaseNum', () => {
+    theNum--;
+    if (theNum < 3) {
+      theNum = 3;
+    }
+    updateDecorationsFunction();
+    vscode.window.showInformationMessage(`theNum decreased to ${theNum}`);
+  });
+
+  // 注册命令到上下文中
+  context.subscriptions.push(increaseNumCommand);
+  context.subscriptions.push(decreaseNumCommand);
+
+
+
+  // 注册快捷键
+  vscode.commands.registerCommand('ConyBlockBrace.registerKeybindings', () => {
+    // 增加数字的快捷键
+    vscode.commands.registerCommand('ConyBlockBrace.increaseNum', () => {
+      theNum++;
+      if (theNum < 3) {
+        theNum = 3;
+      }
+      updateDecorationsFunction();
+      vscode.window.showInformationMessage(`theNum increased to ${theNum}`);
+    });
+    vscode.workspace.getConfiguration().update(
+      'keybindings', [
+      {
+        "key": "ctrl+alt+i",
+        "command": "ConyBlockBrace.increaseNum",
+        "when": "editorTextFocus"
+      }
+    ],
+      vscode.ConfigurationTarget.Global
+    );
+
+    // 减少数字的快捷键
+    vscode.commands.registerCommand('ConyBlockBrace.decreaseNum', () => {
+      theNum--;
+      if (theNum < 3) {
+        theNum = 3;
+      }
+      updateDecorationsFunction();
+      vscode.window.showInformationMessage(`theNum decreased to ${theNum}`);
+    });
+    vscode.workspace.getConfiguration().update(
+      'keybindings', [
+      {
+        "key": "ctrl+alt+k",
+        "command": "ConyBlockBrace.decreaseNum",
+        "when": "editorTextFocus"
+      }
+    ],
+      vscode.ConfigurationTarget.Global
+    );
+    vscode.commands.registerCommand('ConyBlockBrace.ConyToggleHighlighting', () => {
+
+    });
+    vscode.workspace.getConfiguration().update(
+      'keybindings', [
+      {
+        "key": "ctrl+alt+s",
+        "command": "ConyBlockBrace.ConyToggleHighlighting1",
+        "when": "editorTextFocus"
+      }
+    ],
+      vscode.ConfigurationTarget.Global
+    );
+
+  });
+
+
+
+
 
 
   const settingsCommand = vscode.commands.registerCommand('ConyBlockBrace.ConySettings', () => {
@@ -50,17 +158,7 @@ function activate(context) {
   // ];
 
 
-  // 获取用户配置项
-  const config = vscode.workspace.getConfiguration('ConyBlockBrace');
 
-  //获取用户配置的自启动选项
-  highlightingEnabled = config.get('enabled', false); // 默认为 false
-
-  // 获取用户配置的颜色值和是否开启功能的选项
-  const blockColors = config.get('blockColors', []);
-  const blockDecorators = blockColors.map(color => vscode.window.createTextEditorDecorationType({
-    backgroundColor: color
-  }));
 
   let activeEditor = vscode.window.activeTextEditor;
 
@@ -77,6 +175,8 @@ function activate(context) {
     const stack = []; // 用于跟踪代码块的堆栈
     const blockStartLines = {}; // 用于存储每个左大括号所在的行数
     let level = 0; // 代码块层次
+    let blockLevel = 0;
+
 
     let match;
     while ((match = blockRegex.exec(text))) {
@@ -84,7 +184,15 @@ function activate(context) {
       if (match[0] === '{') {
         stack.push({ type: 'start', index: match.index });
         blockStartLines[match.index] = activeEditor.document.positionAt(match.index).line;
+
+
+        blockLevel++;
+
+
       } else {
+
+        level++;
+
         const startMatchInfo = stack.pop();
         const endMatchIndex = match.index;
         const startLine = blockStartLines[startMatchInfo.index];
@@ -99,22 +207,55 @@ function activate(context) {
         const startRange = new vscode.Range(startLineStartPosition.translate(0, startCharacter), startLineStartPosition.translate(0, activeEditor.document.lineAt(startLine).text.length));
         const endRange = new vscode.Range(endLineStartPosition.translate(0, startCharacter), endLineStartPosition.translate(0, endCharacter));
 
-        decorations.push({ range: startRange, hoverMessage: 'Code Block Start', level: level });
-        decorations.push({ range: endRange, hoverMessage: 'Code Block End', level: level });
+        decorations.push({ range: startRange, hoverMessage: 'Code Block Start', level: level, level1: 0, blockLevel: blockLevel, closed: false, blockLevelId: 0 });
+        decorations.push({ range: endRange, hoverMessage: 'Code Block End', level: level, level1: 0, blockLevel: blockLevel, closed: false, blockLevelId: 0 });
 
         // 添加每行第一个字符的底色
-        for (let i = startLine + 1; i <= endLine; i++) {
+        for (let i = startLine + 1; i < endLine; i++) {
           const lineStartPosition = new vscode.Position(i, 0);
           //const lineFirstCharacter = activeEditor.document.lineAt(i).firstNonWhitespaceCharacterIndex;
           const lineRange = new vscode.Range(lineStartPosition.translate(0, startCharacter), lineStartPosition.translate(0, startCharacter + 1));
-          decorations.push({ range: lineRange, hoverMessage: 'Line Start', level: level });
+          decorations.push({ range: lineRange, hoverMessage: 'Line Start', level: level, level1: 0, blockLevel: blockLevel, closed: false, blockLevelId: 0 });
         }
 
-        level = (level + 1) % blockColors.length; // 切换颜色
+        // for (let i = 0; i < decorations.length; i++) {
+        //   if (decorations[i].blockLevel == blockLevel + 1 && decorations[i].closed == false) {
+        //     decorations[i].blockLevelId = level;
+        //     decorations[i].closed = true;
+        //   }
+        // }
+
+        blockLevel--;
       }
     }
+
+
+    let maxLevel = 1;
+    let maxBlockLevel = 0;
+    for (let i = 0; i < decorations.length; i++) {
+      if (decorations[i].level > maxLevel) {
+        maxLevel = decorations[i].level;
+      }
+      if (decorations[i].blockLevel > maxBlockLevel) {
+        maxBlockLevel = decorations[i].blockLevel;
+      }
+    }
+
+
+    for (let i = 1; i <= maxLevel; i++) {
+      for (let j = 0; j < decorations.length; j++) {
+        decorations[j].level1 = maxLevel + 1 - decorations[j].level;
+      }
+    }
+
+
     blockDecorators.forEach((decorator, index) => {
-      activeEditor.setDecorations(decorator, decorations.filter(decoration => decoration.level === index).map(decoration => decoration.range));
+      let num = (theNum - index) % blockColors.length;
+      if (colorMode == true) {
+        activeEditor.setDecorations(decorator, decorations.filter(decoration => decoration.level1 % blockColors.length == num).map(decoration => decoration.range));
+      } else {
+        activeEditor.setDecorations(decorator, decorations.filter(decoration => decoration.blockLevel % blockColors.length == num).map(decoration => decoration.range));
+      }
     });
   }
 
@@ -131,6 +272,10 @@ function activate(context) {
   }
 
 
+
+
+
+
   vscode.window.onDidChangeActiveTextEditor(editor => {
     activeEditor = editor;
     if (editor) {
@@ -144,7 +289,7 @@ function activate(context) {
     }
   });
 
-  blockDecorators.forEach((decorator, index) => {
+  blockDecorators.forEach((decorator) => {
     context.subscriptions.push(decorator);
   });
 }
